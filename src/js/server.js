bc7c0a328eef1afe8971fd7805b816b51a71ca63
Patryk Laba <laba.patryk@gmail.com>
2018-04-01 22:44:05 +0200

    Added server.js which is responsible for rendering components on the server-side

---
+++ src/js/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import path from 'path';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import Html from './components/Html';
+import App from './components/hello';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', async (req, res, next) => {
+    const scripts = ['vendor.js', 'client.js'];
+
+    const appMarkup = ReactDOMServer.renderToString(
+        <Hello initText="rendered on the server" />
+    );
+
+    const html = ReactDOMServer.renderToStaticMarkup(
+        <Html children={appMarkup} scripts={scripts} />
+    );
+
+    res.send(`<!doctype html>${html}`);
+});
+
+app.listen(3000, () => console.log('Listening on :3000'));
