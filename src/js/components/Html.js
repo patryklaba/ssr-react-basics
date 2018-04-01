110bac0a90876b7607eb597203672dd4a8ae3476
Patryk Laba <laba.patryk@gmail.com>
2018-04-01 22:33:47 +0200

    Created placeholder component

---
+++ src/js/components/Html.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Html extends Component {
+    static propTypes = {
+        children: PropTypes.node.isRequired,
+        scripts: PropTypes.array
+    }
+
+    render() {
+        const { children, scripts } = this.props;
+
+        return (
+            <html>
+                <head>
+                    <meta charSet="UTF-8"/>
+                    <title>SSR React App</title>
+                </head>
+                <body>
+                    <div id="app"
+                        dangerouslySetInnerHTML={{ __html: children}}
+                    ></div>
+                    {scripts.map((item, index)=>{
+                        return <script key={index} src={item}></script>
+                    })};
+                </body>
+            </html>
+        );
+    }
+}
+
+
+export default Html;
